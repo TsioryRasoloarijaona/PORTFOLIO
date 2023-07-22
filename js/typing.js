33c9addeb048ed57196575400034a1cad7cbc35f
unknown <hei.tsiory@gmail.com>
2023-07-22 22:36:49 +0300

    header modification

--- js/typing.js
+++ js/typing.js
@@ -1,19 +1,4 @@
-var i = 0 , text ;
-text =  'it is nice to be important but it is more important to  be nice';
 
-
-function typing (){
-   
-    if (i < text.length){
-        document.getElementById('saying').innerText += text.charAt(i);
-        i++;
-    }
-
-
-    setTimeout(typing, 150);
-}
-
-typing();
 
 
 
