4893a2b7c7b48f42165345d28d2c6b1264c869ee
unknown <hei.tsiory@gmail.com>
2023-07-28 00:44:42 +0300

    start experiences

--- js/progression.js
+++ js/progression.js
@@ -1,13 +1,41 @@
-function prog(){const rating = document.getElementsByClassName('rating')[0];
-const block = document.getElementsByClassName('block');
+function prog1(){const rating = document.getElementsByClassName('rating1')[0];
+const block = document.getElementsByClassName('block1');
 
 for (var i = 1 ; i < 100 ; i++){
-    rating.innerHTML+= `<div class="block"></div>`;
+    rating.innerHTML+= `<div class="block1"></div>`;
     block[i].style.transform = "rotate("+3.6 * i + "deg)";
     block[i].style.animationDelay = `${i/40}s`
 }
 
-const counter = document.querySelector('.counter');
+const counter = document.querySelector('.counter1');
+counter.innerText = 0;
+
+const target = +counter.getAttribute('data-target');
+
+const NumberCounter = ()=>{
+    const value = +counter.innerText;
+
+    if(value < target){
+        counter.innerText = Math.ceil(value + 1);
+        setTimeout(()=>{
+            NumberCounter()
+        } , 25)
+    }
+}
+NumberCounter() 
+
+};
+function prog2(){
+    const rating = document.getElementsByClassName('rating2')[0];
+const block = document.getElementsByClassName('block2');
+
+for (var i = 1 ; i < 100 ; i++){
+    rating.innerHTML+= `<div class="block2"></div>`;
+    block[i].style.transform = "rotate("+3.6 * i + "deg)";
+    block[i].style.animationDelay = `${i/40}s`
+}
+
+const counter = document.querySelector('.counter2');
 counter.innerText = 0;
 
 const target = +counter.getAttribute('data-target');
@@ -26,6 +54,122 @@ NumberCounter()
 
 };
 
+function prog3(){
+    const rating = document.getElementsByClassName('rating3')[0];
+const block = document.getElementsByClassName('block3');
+
+for (var i = 1 ; i < 100 ; i++){
+    rating.innerHTML+= `<div class="block3"></div>`;
+    block[i].style.transform = "rotate("+3.6 * i + "deg)";
+    block[i].style.animationDelay = `${i/40}s`
+}
+
+const counter = document.querySelector('.counter3');
+counter.innerText = 0;
+
+const target = +counter.getAttribute('data-target');
+
+const NumberCounter = ()=>{
+    const value = +counter.innerText;
+
+    if(value < target){
+        counter.innerText = Math.ceil(value + 1);
+        setTimeout(()=>{
+            NumberCounter()
+        } , 25)
+    }
+}
+NumberCounter() 
+
+};
+
+function prog4(){
+    const rating = document.getElementsByClassName('rating4')[0];
+const block = document.getElementsByClassName('block4');
+
+for (var i = 1 ; i < 100 ; i++){
+    rating.innerHTML+= `<div class="block4"></div>`;
+    block[i].style.transform = "rotate("+3.6 * i + "deg)";
+    block[i].style.animationDelay = `${i/40}s`
+}
+
+const counter = document.querySelector('.counter4');
+counter.innerText = 0;
+
+const target = +counter.getAttribute('data-target');
+
+const NumberCounter = ()=>{
+    const value = +counter.innerText;
+
+    if(value < target){
+        counter.innerText = Math.ceil(value + 1);
+        setTimeout(()=>{
+            NumberCounter()
+        } , 25)
+    }
+}
+NumberCounter() 
+
+};
+
+
+function prog5(){
+    const rating = document.getElementsByClassName('rating5')[0];
+const block = document.getElementsByClassName('block5');
+
+for (var i = 1 ; i < 100 ; i++){
+    rating.innerHTML+= `<div class="block5"></div>`;
+    block[i].style.transform = "rotate("+3.6 * i + "deg)";
+    block[i].style.animationDelay = `${i/40}s`
+}
+
+const counter = document.querySelector('.counter5');
+counter.innerText = 0;
+
+const target = +counter.getAttribute('data-target');
+
+const NumberCounter = ()=>{
+    const value = +counter.innerText;
+
+    if(value < target){
+        counter.innerText = Math.ceil(value + 1);
+        setTimeout(()=>{
+            NumberCounter()
+        } , 25)
+    }
+}
+NumberCounter() 
+
+};
+
+function prog6(){
+    const rating = document.getElementsByClassName('rating6')[0];
+const block = document.getElementsByClassName('block6');
+
+for (var i = 1 ; i < 100 ; i++){
+    rating.innerHTML+= `<div class="block6"></div>`;
+    block[i].style.transform = "rotate("+3.6 * i + "deg)";
+    block[i].style.animationDelay = `${i/40}s`
+}
+
+const counter = document.querySelector('.counter6');
+counter.innerText = 0;
+
+const target = +counter.getAttribute('data-target');
+
+const NumberCounter = ()=>{
+    const value = +counter.innerText;
+
+    if(value < target){
+        counter.innerText = Math.ceil(value + 1);
+        setTimeout(()=>{
+            NumberCounter()
+        } , 25)
+    }
+}
+NumberCounter() 
+
+};
 
 let lastScrollTop =
 window.pageYOffset || document.body.scrollTop;
@@ -47,13 +191,14 @@ function handleScroll() {
       return direction;
   }
 
+  let skill = document.querySelector('.skill')
 
 window.onscroll = ()=>{
 
     let direction = handleScroll();
     let top = window.scrollY;
-    let offset = card.offsetTop;
-    let height = card.offsetHeight;
+    let offset = skill.offsetTop;
+    let height = skill.offsetHeight;
     let total = window.innerHeight;
  
 
@@ -61,10 +206,17 @@ console.log(document.body.scrollTop);
 
 
     console.log(direction);
+    console.log((offset + height) - total);
+    console.log(top+1);
 
-    if (top >= ((offset + height/1.5) - total) && top<=((offset + height) - total) && direction == true ){
+    if (top >= ((offset + height*4/5) - total) && top <= ((offset + height) - total) && direction == true ){
         console.log('here');
-        prog();
+        prog1();
+        prog2();
+        prog3();
+        prog4();
+        prog5();
+        prog6();
     }
 }
 
